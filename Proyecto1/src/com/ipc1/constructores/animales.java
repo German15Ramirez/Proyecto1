45c072bcb254da8976eaf2a68406676a81d03a1d
German15Ramirez <99780385+German15Ramirez@users.noreply.github.com>
2022-04-13 18:08:44 -0600

    Add files via upload

---
+++ Proyecto1/src/com/ipc1/constructores/animales.java
@@ -0,0 +1,15 @@
+package com.ipc1.constructores;
+
+public class animales {
+    //definimos atributos
+    String nombre;
+    int vida;
+    int ataque;
+    String tipo;
+    //constructores
+    public animales(String nombre, int vida, int ataque, String tipo){
+        this.nombre=nombre;
+        this.vida=vida;
+        
+    }
+}
